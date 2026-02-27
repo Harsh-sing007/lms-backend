712a03b278b58facfe2bb91afe271879af531f55
Harsh <harshsingh45831@gmail.com>
2026-02-27 11:56:26 +0530

    update

--- index.js
+++ index.js
@@ -22,25 +22,18 @@
 // app.use("/api/users", userRouter);
 
 import express from "express";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import dbConnect from "./config/db.js";
 import userRouter from "./routers/userRoute.js";
 import cors from "cors";
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-dotenv.config();
-
-// Routes
-app.use("/api/users", userRouter);
-
-// Start Server
+dotenv.config()
 const startServer = async () => {
   await dbConnect();
   app.listen(8080, () => console.log("Server Started"));
 };
+startServer()
 
-startServer();
+app.use("/api/users", userRouter);
